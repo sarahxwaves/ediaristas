90790b7c200cce34b91c4560efa0d1afb9b6113d
Sarah <sarahmenezesbraga@hotmail.com>
2021-06-10 00:50:38 -0300

    Primeiro commit

---
+++ src/ui/components/data-display/PageTitle/PageTitle.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {PageTitleContainer, PageTitleStyled, PageSubtitleStyled}  from './PageTitle.style';
+
+interface PageTitleProps{
+    title: string;
+    subtitle?: string | JSX.Element;
+}
+
+const PageTitle: React.FC<PageTitleProps> = (props) => {
+
+    return (
+    <div>
+        <PageTitleContainer>
+            <PageTitleStyled>{props.title}</PageTitleStyled>
+            <PageSubtitleStyled>{props.subtitle}</PageSubtitleStyled> 
+        
+        </PageTitleContainer>
+        
+    </div>
+
+    );
+
+}
+
+export default PageTitle;
---
+++ src/ui/components/feedback/SafeEnvironment/SafeEnvironment.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import{ SafeEnvironmentContainer } from './SafeEnvironment.style';
+import{Container} from '@material-ui/core';
+
+const SafeEnvironment = () => {
+    return (<SafeEnvironmentContainer>
+        <Container>
+
+        Ambiente Seguro <i className={'twf-lock'} />
+
+        </Container>
+        </SafeEnvironmentContainer>
+    );
+}
+
+
+export default SafeEnvironment;
