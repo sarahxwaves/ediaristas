d95dd2c70dd43b67de292ee6068edc3459e0f09e
Sarah <sarahmenezesbraga@hotmail.com>
2021-06-12 15:21:44 -0300

    Passagem de parametros com props, importacoes Mask

--- src/ui/components/inputs/TextFieldMask/TextFieldMask.tsx
+++ src/ui/components/inputs/TextFieldMask/TextFieldMask.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import InputMask from 'react-input-mask';
-import TextField from 'ui/components/inputs/TextField/TextField';
-import {OutlinedTextFieldProps} from '@material-ui/core';
+import React from "react";
+import InputMask from "react-input-mask";
+import TextField from "ui/components/inputs/TextField/TextField";
+import { OutlinedTextFieldProps } from "@material-ui/core";
 
-export interface TextFieldMaskProps extends OutlinedTextFieldProps { //pega todas as propriedades q a gnt ja declarou
+export interface TextFieldMaskProps extends OutlinedTextFieldProps {
+  //pega todas as propriedades q a gnt ja declarou
   mask: string;
 }
 
-const TextFieldMask: React.FC<TextFieldMaskProps> = ({mask, ...props}) => {
+const TextFieldMask: React.FC<TextFieldMaskProps> = ({
+  mask,
+  value,
+  onChange,
+  ...props
+}) => {
   //o props recebe as propriedades q a gnt colocou no index, como label, width etc
   return (
-    <InputMask mask={mask}>
+    <InputMask mask={mask} value={value} onChange={onChange}>
       {() => {
-        return <TextField{...props} />;
+        return <TextField {...props} />;
       }}
     </InputMask>
   );
