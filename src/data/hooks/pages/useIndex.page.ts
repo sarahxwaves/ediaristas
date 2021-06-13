18dd2ab1735c6b8b4c57c9bd824cedc0762b0624
Sarah <sarahmenezesbraga@hotmail.com>
2021-06-13 14:00:52 -0300

    Função BuscarProfissionais, lógica de busca e Axios

--- src/data/hooks/pages/useIndex.page.ts
+++ src/data/hooks/pages/useIndex.page.ts
@@ -1,6 +1,9 @@
 import { useState, useMemo } from "react"; //useMemo é pra manter uma funcao sem ficar recalculando ela desnecessariamente
 import { UserShortInterface } from "data/@types/UserInterface";
 import { ValidationService } from "data/services/ValidationService";
+import { ApiService } from "data/services/ApiService";
+
+//hook
 
 export default function useIndex() {
   //pra pegar o CEP do index
@@ -14,9 +17,38 @@ export default function useIndex() {
     [diaristas, setDiaristas] = useState([] as UserShortInterface[]),
     [diaristasRestantes, setDiatistasRestantes] = useState(0); // um array de diaristas com os parametros importados de UserShortInterface
 
+  async function buscarProfissionais(cep: string) {
+    setBuscaFeita(false);
+    setCarregando(true);
+    setErro("");
+
+    try {
+     const {data} = await ApiService.get<{
+       diaristas: UserShortInterface[],
+       quantidade_diaristas: number
+     }>(
+       "/api/diaristas-cidade?cep=" + cep.replace(/\D/g, "")); //await espera executar dps continua
+      setDiaristas(data.diaristas);
+      setDiatistasRestantes(data.quantidade_diaristas);
+
+     
+      setBuscaFeita(true);
+      setCarregando(false);
+    } catch (error) {
+      setErro("CEP não encontrado");
+      setCarregando(false);
+    }
+  }
+
   return {
     cep,
     setCep,
     cepValido,
+    buscarProfissionais,
+    erro,
+    diaristas,
+    buscaFeita,
+    carregando,
+    diaristasRestantes
   };
 }
